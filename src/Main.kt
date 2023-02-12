1d67b0eafd1d94549f875194de10abab356d2428
Aider <aider.dev.work@gmail.com>
2023-02-12 13:00:58 +0300

    Second, third, fourth task completed

--- src/Main.kt
+++ src/Main.kt
@@ -1,52 +1,68 @@
-import Command.*
+import command.Command.*
+import model.Hint
+import model.Manager
+import model.Parking
 
 var parkingOpen: Boolean = true
 
-fun main() {
-    printlnHint(Hint.BEFORE_USING)
+var manager: Manager? = null
 
+fun main() {
+    Parking.carParkingInit()
+
+    manager = Manager()
+
+    var enteredRequest = ""
     while (parkingOpen){
-        sendRequest(readLine().toString())
+        println("\nВведите команду:")
+        enteredRequest = readLine().toString()
+
+        sendRequest(enteredRequest.split(" ")[0], enteredRequest)
     }
 }
 
-fun sendRequest(command: String){
+fun sendRequest(command: String, allStringCommand: String){
     when(command){
+        PARK.command -> {
+            manager?.addCarToPark(allStringCommand)
+        }
+
+        RETURN.command -> {
+            manager?.returnCar(allStringCommand)
+        }
+
+        PARK_BY_PLACE.command -> {
+            manager?.parkInfoByPlace(allStringCommand)
+        }
+
+        PARK_BY_CAR.command -> {
+            manager?.parkInfoByCar(allStringCommand)
+        }
+
+        PARK_STATS.command -> {
+            manager?.getParkStats()
+        }
+
+        PARK_ALL_STATS.command -> {
+            manager?.getAllParkStats()
+        }
+
         START.command -> {
-            printlnHint(Hint.GREETINGS)
+            println(Hint.GREETINGS)
         }
 
         HELP.command -> {
-            printlnHint(Hint.HELP)
+            println(Hint.HELP)
         }
 
         END.command -> {
-            printlnHint(Hint.COMPLETION)
+            println(Hint.COMPLETION)
 
             parkingOpen = false
         }
 
         else -> {
-            printlnHint(Hint.ERROR)
+            println(Hint.ERROR)
         }
     }
-}
-
-fun printlnHint(hint: String) {
-    println(hint)
-}
-
-
-enum class Command(val command: String) {
-    START("/start"),
-    HELP("/help"),
-    END("/end")
-}
-
-object Hint {
-    const val BEFORE_USING = "Please enter command => /start, /help, /end"
-    const val GREETINGS = "Welcome to the \"Car parking program\""
-    const val HELP = "Available commands /start, /end"
-    const val ERROR = "The program cannot process the request... Call the '/help' command"
-    const val COMPLETION = "Thank you for using our program\nWe strive to make our product better and more convenient for users"
 }
